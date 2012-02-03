9dcd706189ef6558e70b652677870d74c5a0850c
ileonkina <irinka-leo@yandex.ru>
2012-02-03 16:08:40 +0400

    2 commit

--- github.py
+++ github.py
@@ -1,25 +1,21 @@
 import requests
 import json
+
 try:
-    f = open('conf','r')
+    f = open('conf.py','r')
 except IOError:
     print "File 'conf' not found"
     exit()
-lin = f.readlines()
-f.close()
-st = lin[0].split("=")[1]
-login = st[0:len(st)-1]
-st = lin[1].split("=")[1]
-passw = st[0:len(st)-1]
-st = lin[2].split("=")[1]
-org_name = st[0:len(st)-1]
+from conf import password
+from conf import login
+from conf import org_name
 host = 'https://api.github.com/'
 
 #create team
 def create_team(team_name,permission,repo_name):
 	reqq = 'orgs/%s/teams' % org_name
 	url = host + reqq
-	r = requests.post(url,auth = (login,passw),data = '{"name":"%s", "repo_names":["%s/%s"], "permission":"%s"}' % (team_name,org_name,repo_name,permission))
+	r = requests.post(url,auth = (login,password),data = '{"name":"%s", "repo_names":["%s/%s"], "permission":"%s"}' % (team_name,org_name,repo_name,permission))
 	if r.status_code != 201:
 		res = "Error "+ r.headers['status']
 	else:
@@ -30,7 +26,7 @@ def create_team(team_name,permission,repo_name):
 def create_repo(repo_name,private,description):
     reqq='orgs/%s/repos' % (org_name)
     url = host + reqq
-    r = requests.post(url, auth=(login,passw),data = '{"name":"%s","private":"%s","description":"%s"}' % (repo_name,private,description))
+    r = requests.post(url, auth=(login,password),data = '{"name":"%s","private":"%s","description":"%s"}' % (repo_name,private,description))
     if r.status_code == 201:
         res = "Done! Repo %s created" % repo_name
         # creating 3 teams
@@ -43,28 +39,28 @@ def create_repo(repo_name,private,description):
 
 #search id_team by name
 def search_id_team(team_name):
-    reqq = 'orgs/%s/teams' % (org_name)
-    url = host + reqq
-    r = requests.get(url, auth = (login,passw))
-    cont = json.loads(r.content)
-    i = 0
-    while 1:
-        try:
-            if cont[i]['name'] == team_name:
-                break
-            i += 1
-        except:
-            return "Team not found"
-    return cont[i]['id']
+	reqq = 'orgs/%s/teams' % org_name
+	url = host + reqq
+	r = requests.get(url, auth = (login,password))
+	cont = json.loads(r.content)
+	i = 0
+	while 1:
+		try:
+			if cont[i]['name'] == team_name:
+				break
+			i += 1
+		except:
+			return "Team not found"
+	return cont[i]['id']
 
 #add user to team
 def add_user_to_team(user,team_name):
-	team_id = str(search_id_team(team_name))
-	if team_id == "Team not found":
+	team_id = search_id_team(team_name)
+	if team_id == -1:
 		return "Team not found"
-	reqq = 'teams/%s/members/%s' % (team_id,user)
+	reqq = 'teams/%d/members/%s' % (team_id,user)
 	url = host + reqq
-	r = requests.put(url,auth = (login,passw),data = '{"login":"%s"}' % user)
+	r = requests.put(url,auth = (login,password),data = '{"login":"%s"}' % user)
 	if r.status_code != 204:
 		res = "Error "+ r.headers['status']
 	else:
@@ -75,9 +71,9 @@ def add_user_to_team(user,team_name):
 def del_user_from_team(user,team_name):
     if search_id_team(team_name) == "Team not found":
         return "Team not found"
-    reqq = 'teams/%s/members/%s' % (str(search_id_team(team_name)),user)
+    reqq = 'teams/%d/members/%s' % (search_id_team(team_name),user)
     url = host + reqq
-    r = requests.delete(url, auth = (login,passw))
+    r = requests.delete(url, auth = (login,password))
     if r.status_code == 204:
         res = "User '" + user + "' was deleted from team " + team_name
     else:
@@ -88,7 +84,7 @@ def del_user_from_team(user,team_name):
 def del_user_from_org(user):
 	reqq = 'orgs/%s/members/%s' % (org_name,user)
 	url = host + reqq
-	r = requests.delete(url,auth = (login,passw))
+	r = requests.delete(url,auth = (login,password))
 	if r.status_code != 204:
 		res = "Error "+ r.headers['status']
 	else:
