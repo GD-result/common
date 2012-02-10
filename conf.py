6e9eca6f166b079f93492c2de193f232ac0e31e8
shoma <mustafin_shamil@yahoo.com>
2012-02-10 16:41:45 +0400

    qwer

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 login = '12121212121212'
 password = '121'
 org_name = 'GD-result'
-debug = True
-type_pass = False
+debug = False
+type_pass = True
 token = "56454eae8676f36e2fd2d0c9157b05aff6cb0818"
